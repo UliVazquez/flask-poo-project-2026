90f531843fe3b73568cf75968e51bea9e008fce9
UliVazquez <uliv.contacto@gmail.com>
2026-06-24 11:45:53 -0300

    Added bootstrap and rdefined .html files

--- app.py
+++ app.py
@@ -1,25 +1,18 @@
-from flask import Flask, redirect, url_for, render_template, request
+from flask import Flask, redirect, url_for, render_template, request, session
 
 app = Flask(__name__)
+app.secret_key = "profeapruebemeporfavor"
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/enviar-trabajo', methods=['POST', 'GET'])
+@app.route('/upload')
 def enviar_trabajo():
-    return render_template('envia_trabajos.html')
+    return render_template('upload.html')
 
-@app.route("/login", methods=["GET", "POST"])
+@app.route("/login")
 def login():
-
-    if request.method == "POST":
-
-        correo = request.form["correo"]
-        password = request.form["password"]
-
-        # buscar organizador en la BD
-
     return render_template("login.html")
 
 if __name__ == '__main__':
